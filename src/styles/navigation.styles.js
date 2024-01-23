1aeefc7708f58bb9fc17182414974c547a979deb
andrewandyle <andrewandyle@gmail.com>
2024-01-23 17:47:49 -0600

    get rid of glass effect on mobile

--- src/styles/navigation.styles.js
+++ src/styles/navigation.styles.js
@@ -38,15 +38,17 @@ export const NavContainer = styled.header`
     }
   }
 
-  &:before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: -100%;
-    width: 100%;
-    height: 100%;
-    background: linear-gradient(90deg, transparent, rgba(255, 255, 255, .4), transparent);
-    transition: .5s;
+  @media only screen and (min-width: 721px) {
+    &:before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: -100%;
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(90deg, transparent, rgba(255, 255, 255, .4), transparent);
+      transition: .5s;
+    }
   }
 
   &:hover {
