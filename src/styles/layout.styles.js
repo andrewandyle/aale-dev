43f4421a0e260d2f9aeecfffce3371b5df8a5083
andrewandyle <andrewandyle@gmail.com>
2024-01-22 21:30:21 -0600

    update nav and site bg

--- src/styles/layout.styles.js
+++ src/styles/layout.styles.js
@@ -1,9 +1,11 @@
 import { styled, createGlobalStyle } from 'styled-components'
+import imageBg from '../images/bg.jpg'
 
 export const OuterContainer = styled.div`
   margin: 0 auto;
   max-width: 960px;
   padding: 0 1.0875rem 1.45rem;
+  padding-top: 80px;
 `
 
 export const PageContainer = styled.div`
@@ -14,7 +16,10 @@ export const PageContainer = styled.div`
 
 export const GlobalStyle = createGlobalStyle`
   body {
-    background: linear-gradient(76.8deg, rgb(121, 45, 129) 3.6%, rgb(36, 31, 98) 90.4%);
+    background-image: linear-gradient(
+      rgba(0, 0, 0, 0.5), 
+      rgba(0, 0, 0, 0.5)
+    ), url(${imageBg});
     background-attachment: fixed;
     color: white;
     font-family: 'Ubuntu', sans-serif;
--- src/styles/navigation.styles.js
+++ src/styles/navigation.styles.js
@@ -3,6 +3,17 @@ import { GlassContainer } from './index.styles'
 
 export const NavContainer = styled(GlassContainer)`
   margin-bottom: 50px;
+  
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  z-index: 9999;
+
+  border-radius: 0;
+  border: none;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.4);
+  backdrop-filter: blur(10px);
 
   a {
     color: white;
@@ -15,5 +26,32 @@ export const NavContainer = styled(GlassContainer)`
     justify-content: space-evenly;
     padding-inline-start: 0px;
     font-weight: 500;
+
+    li {
+      -webkit-transform: scale(1.0);
+      transition: 500ms;
+      
+      &:hover {
+        -webkit-transform: scale(1.3);
+        transition: 500ms;
+      }
+    }
+  }
+
+  &:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: -100%;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(90deg, transparent, rgba(255, 255, 255, .4), transparent);
+    transition: .5s;
+  }
+
+  &:hover {
+    &:before {
+      left: 100%;
+    }
   }
 `
